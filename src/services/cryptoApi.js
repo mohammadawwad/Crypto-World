2859ff7f7f03cc84773a0525b8e2f3b4618c7d06
mohammadawwad <mohd-awwad@hotmail.com>
2022-02-13 11:22:16 -0800

    update

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 
-//need to switch to v2 of the api
+//need to switch to v2 of the api already changed the key and base url need to change the rest
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': 'coinranking22a820aba46dc7cd6e2e28af484ba15587c070b42a371444'
